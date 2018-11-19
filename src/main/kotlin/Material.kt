8ad7689a4f04f7961473f140fa38e941f8ded9ae
Edwin Jakobs <edwin@rndr.studio>
2018-11-19 22:15:17 +0100

    More steps for the render passes, facets and combiners

--- src/main/kotlin/Material.kt
+++ src/main/kotlin/Material.kt
@@ -1,6 +1,7 @@
 package org.openrndr.dnky
 
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.RenderTarget
 import org.openrndr.draw.ShadeStyle
 import org.openrndr.draw.shadeStyle
 import org.openrndr.math.Vector2
@@ -24,10 +25,10 @@ private fun PointLight.fs(index: Int): String = """
 |   vec3 dpn = normalize(dp);
 |
 |   float side = dot(dpn, wnn);
-|   light += attenuation * max(0, side) * p_lightColor$index.rgb * p_diffuse.rgb;
+|   f_diffuse += attenuation * max(0, side) * p_lightColor$index.rgb * p_diffuse.rgb;
 |   if (side > 0.0) {
 |       float f = max(0.0, dot(reflect(-dpn, wnn), edn));
-|       light += attenuation * pow(f, p_shininess) * p_lightColor$index.rgb * p_specular.rgb;
+|       f_specular += attenuation * pow(f, p_shininess) * p_lightColor$index.rgb * p_specular.rgb;
 |   }
 }
 """.trimMargin()
@@ -37,26 +38,27 @@ private fun AmbientLight.fs(index: Int): String = "light += p_lightColor$index.r
 private fun DirectionalLight.fs(index: Int) = """
 |{
 |    float side = dot(-wnn, p_lightDirection$index);
-|    light += max(0, side) * p_lightColor$index.rgb * p_diffuse.rgb;
+|    f_diffuse += max(0, side) * p_lightColor$index.rgb * p_diffuse.rgb;
 |    if (side > 0.0) {
 |        float f = max(0.0, dot(reflect(p_lightDirection$index, wnn), edn));
-|        light += pow(f, p_shininess) * p_lightColor$index.rgb * p_specular.rgb;
+|        f_specular += pow(f, p_shininess) * p_lightColor$index.rgb * p_specular.rgb;
 |    }
 |}
 """.trimIndent()
 
-private fun HemisphereLight.fs(index :Int): String = """
+private fun HemisphereLight.fs(index: Int): String = """
 |{
 |   float f = dot(wnn, p_lightDirection$index) * 0.5 + 0.5;
-|   vec3 irr = ${irradianceMap?.let { "texture(p_lightIrradianceMap$index, wnn).rgb" }?: "vec3(1.0)"};
-|   light += mix(p_lightDownColor$index.rgb, p_lightUpColor$index.rgb, f) * irr;
+|   vec3 irr = ${irradianceMap?.let { "texture(p_lightIrradianceMap$index, wnn).rgb" } ?: "vec3(1.0)"};
+|   f_diffuse += mix(p_lightDownColor$index.rgb, p_lightUpColor$index.rgb, f) * irr;
 |}
 """.trimIndent()
 
 private fun Fog.fs(index: Int): String = """
 |{
 |    float dz = min(1.0, -v_viewPosition.z/p_fogEnd$index);
-|    light = mix(light, p_fogColor$index.rgb, dz);
+|    f_diffuse = mix(f_diffuse, 0.5 * p_fogColor$index.rgb, dz);
+|    f_specular = mix(f_specular, 0.5 * p_fogColor$index.rgb, dz);
 |}
 """.trimIndent()
 
@@ -69,9 +71,12 @@ class BasicMaterial : Material {
     var parameters = mutableMapOf<String, Any>()
 
     override fun generateShadeStyle(context: MaterialContext): ShadeStyle {
+        val needLight = needLight(context)
+
         val lights = context.lights
-        val fs = """
-        vec3 light = vec3(0.0);
+        val lightFS = if (needLight) """
+        vec3 f_diffuse = vec3(0.0);
+        vec3 f_specular = vec3(0.0);
         vec3 wnn = normalize(v_worldNormal);
         vec3 ep = (p_viewMatrixInverse * vec4(0.0, 0.0, 0.0, 1.0)).xyz;
         vec3 edn = normalize(ep - v_worldPosition);
@@ -89,14 +94,38 @@ class BasicMaterial : Material {
         ${context.fogs.mapIndexed { index, (node, fog) ->
             fog.fs(index)
         }.joinToString("\n")}
-        x_fill.rgb = light;
-        x_fill.a = 1.0;
-    """.trimIndent()
+
+        ${context.pass.combiners.joinToString("\n") {
+            it.generateShader()
+        }
+        }
+    """.trimIndent() else ""
+
+        val rt = RenderTarget.active
+
+        val combinerFS = context.pass.combiners.map {
+            it.generateShader()
+        }.joinToString("\n")
+
+        val fs = lightFS + combinerFS
+
+
 
         return shadeStyle {
+            this.suppressDefaultOutput = true
             this.vertexTransform = this@BasicMaterial.vertexTransform
-            fragmentTransform = fs
+            fragmentTransform = lightFS
+            context.pass.combiners.map {
+                this.output(it.targetOutput, rt.colorBufferIndex(it.targetOutput))
+            }
         }
+    }
+
+    private fun needLight(context: MaterialContext): Boolean {
+        val needSpecular = context.pass.combiners.any { FacetType.SPECULAR in it.facets }
+        val needDiffuse = context.pass.combiners.any { FacetType.SPECULAR in it.facets }
+        val needLight = needSpecular || needDiffuse
+        return needLight
     }
 
     override fun applyToShadeStyle(context: MaterialContext, shadeStyle: ShadeStyle) {
@@ -104,49 +133,51 @@ class BasicMaterial : Material {
         shadeStyle.parameter("diffuse", diffuse)
         shadeStyle.parameter("shininess", shininess)
 
-        parameters.forEach { k, v ->
-            when (v) {
-                is Double -> shadeStyle.parameter(k, v)
-                is Int -> shadeStyle.parameter(k, v)
-                is Vector2 -> shadeStyle.parameter(k, v)
-                is Vector3 -> shadeStyle.parameter(k, v)
-                is Vector4 -> shadeStyle.parameter(k, v)
-                else -> TODO("support ${v::class.java}")
+        if (needLight(context)) {
+            parameters.forEach { k, v ->
+                when (v) {
+                    is Double -> shadeStyle.parameter(k, v)
+                    is Int -> shadeStyle.parameter(k, v)
+                    is Vector2 -> shadeStyle.parameter(k, v)
+                    is Vector3 -> shadeStyle.parameter(k, v)
+                    is Vector4 -> shadeStyle.parameter(k, v)
+                    else -> TODO("support ${v::class.java}")
+                }
             }
-        }
 
-        val lights = context.lights
-        lights.forEachIndexed { index, (node, light) ->
-            shadeStyle.parameter("lightColor$index", light.color)
-            when (light) {
-                is AmbientLight -> {
-                }
+            val lights = context.lights
+            lights.forEachIndexed { index, (node, light) ->
+                shadeStyle.parameter("lightColor$index", light.color)
+                when (light) {
+                    is AmbientLight -> {
+                    }
 
-                is PointLight -> {
-                    shadeStyle.parameter("lightPosition$index", (node.worldTransform * Vector4.UNIT_W).xyz)
-                    shadeStyle.parameter("lightConstantAttenuation$index", light.constantAttenuation)
-                    shadeStyle.parameter("lightLinearAttenuation$index", light.linearAttenuation)
-                    shadeStyle.parameter("lightQuadraticAttenuation$index", light.quadraticAttenuation)
-                }
+                    is PointLight -> {
+                        shadeStyle.parameter("lightPosition$index", (node.worldTransform * Vector4.UNIT_W).xyz)
+                        shadeStyle.parameter("lightConstantAttenuation$index", light.constantAttenuation)
+                        shadeStyle.parameter("lightLinearAttenuation$index", light.linearAttenuation)
+                        shadeStyle.parameter("lightQuadraticAttenuation$index", light.quadraticAttenuation)
+                    }
 
-                is DirectionalLight -> {
-                    shadeStyle.parameter("lightDirection$index", ((normalMatrix(node.worldTransform)) * light.direction).normalized)
-                }
+                    is DirectionalLight -> {
+                        shadeStyle.parameter("lightDirection$index", ((normalMatrix(node.worldTransform)) * light.direction).normalized)
+                    }
 
-                is HemisphereLight -> {
-                    shadeStyle.parameter("lightDirection$index", ((normalMatrix(node.worldTransform)) * light.direction).normalized)
-                    shadeStyle.parameter("lightUpColor$index", light.upColor)
-                    shadeStyle.parameter("lightDownColor$index", light.downColor)
+                    is HemisphereLight -> {
+                        shadeStyle.parameter("lightDirection$index", ((normalMatrix(node.worldTransform)) * light.direction).normalized)
+                        shadeStyle.parameter("lightUpColor$index", light.upColor)
+                        shadeStyle.parameter("lightDownColor$index", light.downColor)
 
-                    light.irradianceMap?.let {
-                        shadeStyle.parameter("lightIrradianceMap$index", it)
+                        light.irradianceMap?.let {
+                            shadeStyle.parameter("lightIrradianceMap$index", it)
+                        }
                     }
                 }
             }
-        }
-        context.fogs.forEachIndexed { index, (node, fog) ->
-            shadeStyle.parameter("fogColor$index", fog.color)
-            shadeStyle.parameter("fogEnd$index", fog.end)
+            context.fogs.forEachIndexed { index, (node, fog) ->
+                shadeStyle.parameter("fogColor$index", fog.color)
+                shadeStyle.parameter("fogEnd$index", fog.end)
+            }
         }
     }
 }
--- src/main/kotlin/SceneRenderer.kt
+++ src/main/kotlin/SceneRenderer.kt
@@ -3,31 +3,37 @@ package org.openrndr.dnky
 import org.openrndr.draw.*
 import org.openrndr.math.Matrix44
 
-abstract class RenderFacet(val targetOutput: String, val format: ColorFormat, val type: ColorType) {
+enum class FacetType(val shaderFacet:String) {
+    WORLD_POSITION("f_worldPosition"),
+    WORLD_NORMAL("f_worldNormal"),
+    SPECULAR("f_specular"),
+    DIFFUSE("f_diffuse")
+}
+
+abstract class RenderFacet(val facets: Set<FacetType>, val targetOutput: String, val format: ColorFormat, val type: ColorType) {
     abstract fun generateShader(): String
 }
 
-class PositionFacet : RenderFacet("position", ColorFormat.RGB, ColorType.FLOAT16) {
+class PositionFacet : RenderFacet(setOf(FacetType.WORLD_POSITION),"position", ColorFormat.RGB, ColorType.FLOAT16) {
     override fun generateShader(): String = "o_$targetOutput.rgb = v_worldPosition.rgb;"
 }
 
-class NormalFacet : RenderFacet("normal", ColorFormat.RGB, ColorType.FLOAT16) {
+class NormalFacet : RenderFacet(setOf(FacetType.WORLD_NORMAL), "normal", ColorFormat.RGB, ColorType.FLOAT16) {
     override fun generateShader() = "o_$targetOutput.rgb = v_worldNormal.rgb;"
 }
 
-class LDRColorFacet : RenderFacet("color", ColorFormat.RGBa, ColorType.UINT8) {
-    override fun generateShader() = "o_$targetOutput.rgb = f_diffuse.rgb + f_specular.rgb;"
+class LDRColorFacet : RenderFacet(setOf(FacetType.DIFFUSE, FacetType.SPECULAR), "color", ColorFormat.RGBa, ColorType.UINT8) {
+    override fun generateShader() = "o_$targetOutput.rgba = vec4(f_diffuse.rgb + f_specular.rgb, 1.0);"
 }
 
-
-class RenderPass(val facets: List<RenderFacet>)
+class RenderPass(val combiners: List<RenderFacet>)
 
 val DefaultPass = RenderPass(listOf(LDRColorFacet()))
 val LightPass = RenderPass(listOf(PositionFacet()))
 
-fun createPassTarget(pass: RenderPass, width: Int, height: Int) {
-    val rt = renderTarget(width, height) {
-        for (facet in pass.facets) {
+fun createPassTarget(pass: RenderPass, width: Int, height: Int) : RenderTarget {
+    return renderTarget(width, height) {
+        for (facet in pass.combiners) {
             colorBuffer(facet.targetOutput, facet.format, facet.type)
         }
         depthBuffer()
